b2e2385fe0628eb7cacc91d2c21675846ef6dc75
TharunBalaji2004 <tharunbalaji31@gmail.com>
2023-12-11 14:20:38 +0530

    Bug fixes in Home component

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./Navbar";
+import Table from "./Table";
+
+const Home = () => {
+    return (
+        <>
+            <Navbar isVisible={true} />
+            <Table />
+        </>
+    );
+}
+
+export default Home;
