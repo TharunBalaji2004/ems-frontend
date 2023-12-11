1dddd33afbdb57ea0372f775644857d75e1db300
TharunBalaji2004 <tharunbalaji31@gmail.com>
2023-12-11 14:00:12 +0530

    Added error page

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -7,12 +7,14 @@ const Error = () => {
             <Navbar isVisible={false} />
             
             <div className="flex justify-center p-10">
-                <p className="text-xl font-bold">404: Page not found</p>
+                
+                <p className="text-lg font-bold">404: Page not found</p>
 
                 <Link to="/">
-
                     <button className="px-4 py-1 rounded-3xl bg-white text-sky-500 font-bold shadow-2xl">Go Home</button>
                 </Link>
+
+
             </div>
         </>
     );
